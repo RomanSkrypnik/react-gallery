2551e0e91083b7d1657faf78242ff668b12e69e6
unknown <rorchenko@gmail.com>
2022-09-06 16:23:26 +0300

    image preview

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppDispatch } from '../hooks';
+import { fetchImages } from '../store/slices/gallery';
+import { useAppSelector } from '../hooks/useAppSelector';
 
 export const HomePage = () => {
+
+    const dispatch = useAppDispatch();
+
+    const { items } = useAppSelector(state => state.gallery);
+
+    useEffect(() => {
+        dispatch(fetchImages());
+    }, []);
+
     return (
-        <div>
-            home
-        </div>
+        <section className='home'>
+            {
+                items.map(({ urls }) => <img src={urls.small} alt='' />)
+            }
+        </section>
     );
 };
